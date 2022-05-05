987dcdfd739c58f6cb3ccf245a6ca994da6a3fff
JacquelineMalatare <jacqueline.malatare@gmail.com>
2022-05-04 20:36:07 -0600

    commit tree 3920d5c394c694802bda22f5bad2772a1ea47258

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Code your solution in this file!
+let companyName = 'Scuber'
+const fs = require ('fs')
+const path = require('path')
+
+const js = fs.readFileSynce(path.resolve(_dirname, '..','index.js'), 'utf-8')
