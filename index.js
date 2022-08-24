8624b61676a8c05415d3f87a1d4f23f7bae50273
JacquelineMalatare <jacqueline.malatare@gmail.com>
2022-08-23 19:49:59 -0600

    update repository

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Code your solution in this file!
+const { expect } = require("chai")
+
+let companyName = 'Scuber'
+const it = 'Scuber'
+expect mostprofitableneighborhood = Chelsea
+let companyCeo = Susan Smith
--- test/indexTest.js
+++ test/indexTest.js
@@ -8,29 +8,17 @@ describe('index.js', function () {
     it('is set as Scuber', function () {
       expect(companyName).to.equal('Scuber');
     });
-
     it('is defined as a const', function () {
       expect(js).to.match(/const companyName/, "Expected companyName to be a const");
     });
   });
-
   describe('mostProfitableNeighborhood', function () {
     it('is declared as equal to Chelsea', function () {
       expect(mostProfitableNeighborhood).to.equal('Chelsea');
     });
-
     it('is defined using let', function () {
       expect(js).to.match(/let mostProfitableNeighborhood/, "Expected mostProfitableNeighborhood to be defined using let");
     });
   });
-
   describe('companyCeo', function () {
-    it('is declared as equal to Susan Smith', function () {
-      expect(companyCeo).to.equal('Susan Smith');
-    });
-
-    it('is defined using let', function () {
-      expect(js).to.match(/let companyCeo/, "Expected companyCeo to be defined using let");
-    });
-  });
-});
+    it('is declared as equal to Susan Smith', function ()
